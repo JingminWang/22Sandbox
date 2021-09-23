208cdc91e69fc0fa2a6ae302ad9e789a533f22f3
JingminWang <3202241005@stu.xaut.edu.cn>
2021-09-23 23:55:28 +0800

    Add password check program

---
+++ password_check.py
@@ -0,0 +1,23 @@
+Min_length=5
+def main():
+    password = get_password()
+    while not is_valid_password(password):
+        print("invalid password")
+        Print_asterisks(password)
+        print()
+        password = input(">")
+    print("{} is valid ".format(password))
+    Print_asterisks(password)
+def get_password():
+   return input(">")
+
+
+def Print_asterisks(password):
+    for i in range(len(password)):
+        print('*', end='')
+def is_valid_password(password):
+    if len(password)<Min_length:
+        return False
+    else:
+        return True
+main()
